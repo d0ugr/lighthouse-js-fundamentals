b159b59bf6fcb1c1c20732448f8cf6825a95a711
d0ugr <doug@escm.ml>
2020-01-18 15:50:28 +0000

    Added age-calculator.js

---
+++ age-calculator.js
@@ -0,0 +1,17 @@
+
+
+
+function ageCalculator(name, yearOfBirth, currentYear)
+{
+   return name + " is " + (currentYear - yearOfBirth) + " years old."
+}
+
+console.log(ageCalculator("Suzie", 1983, 2015));
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
+
+console.log(ageCalculator("Doug", "nope", 2020));
+
+
+
